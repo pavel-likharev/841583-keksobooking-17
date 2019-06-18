abb327be03889b0edbbd591c5b9b5537a39a12cf
PavelLikharev <pavel7321@mail.ru>
2019-06-18 21:54:36 +0700

    Устранил замечания и доработал генерирование массива

--- js/main.js
+++ js/main.js
@@ -1,43 +1,28 @@
 'use strict';
-// Задаём массив с индексами адресов картинок
-var adressImage = ['01', '02', '03', '04', '05', '06', '07', '08'];
-
-// Задаём массив с вариантами квартир и функцию рандомного получения варианта
+// Задаём константы
+var AMOUNT_OFFERS = 8;
+var SIZE_PIN = 40;
+var MAP_HEIGHT_MIN = 130 + SIZE_PIN;
+var MAP_HEIGHT_MAX = 630 + SIZE_PIN;
+var MAP_WIDTH_MIN = document.querySelector('.map__overlay').offsetLeft + SIZE_PIN;
+var MAP_WIDTH_MAX = document.querySelector('.map__overlay').offsetWidth - SIZE_PIN;
+// Задаём массив типов предложений и функцию случайной выдачи типа
 var typeOffer = ['palace', 'flat', 'house', 'bungalo'];
 function getTypeOffer() {
   return Math.floor(Math.random() * (typeOffer.length - 0)) + 0;
 }
 // Задаём функции с определением координат местоположения
-var sizeMap = document.querySelector('.map__overlay').offsetWidth;
 function getLocationX() {
-  return Math.floor(Math.random() * (sizeMap - 0 + 1)) + 0;
+  return Math.floor(Math.random() * (MAP_WIDTH_MAX - MAP_WIDTH_MIN + 1)) + MAP_WIDTH_MIN;
 }
 function getLocationY() {
-  return Math.floor(Math.random() * (630 - 130 + 1)) + 130;
+  return Math.floor(Math.random() * (MAP_HEIGHT_MAX - MAP_HEIGHT_MIN + 1)) + MAP_HEIGHT_MIN;
 }
-// Задаём функцию генерации случайных данных для маркеров
-function generateListOffers(listNumberOffers) {
-  for (var j = 0; j < 8; j++) {
-    if (listNumberOffers === j) {
-      return {
-        author: {
-          avatar: 'img/avatars/user' + adressImage[j] + '.png'
-        },
-        offer: {
-          type: typeOffer[getTypeOffer()]
-        },
-        location: {
-          x: getLocationX(),
-          y: getLocationY()
-        }
-      };
-    }
-  }
-  // Линтер ругается, что return должен быть в конце функции,
-  // а если объект завернуть в переменную, то ломается массив
-  return {
+// Задаём функцию генерации случайных данных для одного маркера
+function generateRandomOffer() {
+  var randomOffer = {
     author: {
-      avatar: 'img/avatars/user' + adressImage[j] + '.png'
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
     },
     offer: {
       type: typeOffer[getTypeOffer()]
@@ -47,18 +32,24 @@ function generateListOffers(listNumberOffers) {
       y: getLocationY()
     }
   };
+  return randomOffer;
 }
 // Формируем массив с помощью функции генерации
-var offers = [generateListOffers(0), generateListOffers(1), generateListOffers(2), generateListOffers(3), generateListOffers(4), generateListOffers(5), generateListOffers(6), generateListOffers(7)];
+var offers = [];
+for (var i = 0; i < AMOUNT_OFFERS; i++) {
+  var generatedOffer = generateRandomOffer();
+  offers[i] = generatedOffer;
+}
 // Открываем карту
 document.querySelector('.map').classList.remove('map--faded');
 var listPins = document.querySelector('.map__pins');
 
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-var pinImage = pin.querySelector('img');
+
 // Задаём функцию создания одного маркера
 var renderPin = function (offer) {
   var pinElement = pin.cloneNode(true);
+  var pinImage = pinElement.querySelector('img');
 
   pinElement.style = 'left: ' + offer.location.x + 'px; top: ' + offer.location.y + 'px;';
   pinImage.src = offer.author.avatar;
@@ -70,8 +61,9 @@ var renderPin = function (offer) {
 var createListOffers = function () {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < offers.length; i++) {
-    fragment.appendChild(renderPin(offers[i]));
+  for (var j = 0; j < offers.length; j++) {
+    var generatedPin = renderPin(offers[j]);
+    fragment.appendChild(generatedPin);
   }
 
   listPins.appendChild(fragment);
